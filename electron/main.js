604eb1e225068fe9e9f04d437867012593da72a6
MatExAi <exarhakou@gmail.com>
2025-12-22 08:39:06 +0200

    Create main.js

---
+++ electron/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    icon: path.join(__dirname, '../build/icon.png')
+  });
+
+  // Load the built web app
+  const startUrl = path.join(__dirname, '../dist/web/index.html');
+  mainWindow.loadFile(startUrl);
+
+  // Open DevTools in development
+  // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
